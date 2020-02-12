ea8f6c06b39db27c85ee9b65bda3d3aaee2b89e7
TraceDecker <decker1041@gmail.com>
2020-02-11 21:36:51 -0700

    added database class.

---
+++ app/src/main/java/edu/cnm/deepdive/thewatcher/services/TheWatcherDatabase.java
@@ -0,0 +1,48 @@
+package edu.cnm.deepdive.thewatcher.services;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import edu.cnm.deepdive.thewatcher.model.dao.AppDao;
+import edu.cnm.deepdive.thewatcher.model.dao.LocationDao;
+import edu.cnm.deepdive.thewatcher.model.dao.PolicyDao;
+import edu.cnm.deepdive.thewatcher.model.entity.App;
+import edu.cnm.deepdive.thewatcher.model.entity.Location;
+import edu.cnm.deepdive.thewatcher.model.entity.Policy;
+
+@Database(
+    entities = {App.class, Policy.class, Location.class},
+    version = 1,
+    exportSchema = true
+)
+
+public abstract class TheWatcherDatabase extends RoomDatabase {
+
+  private static final String DB_NAME = "watcher_db";
+  private static Application context;
+
+  public static TheWatcherDatabase getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public abstract AppDao getAppDao();
+
+  public abstract PolicyDao getPolicyDao();
+
+  public abstract LocationDao getLocationDao();
+
+  public static void setContext(Application context) {
+    TheWatcherDatabase.context = context;
+  }
+
+  private static class InstanceHolder {
+
+
+    private static final TheWatcherDatabase INSTANCE = Room.databaseBuilder(
+        context, TheWatcherDatabase.class, DB_NAME)
+        .build();
+
+  }
+
+}
